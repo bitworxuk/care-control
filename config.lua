83c794a6f5d41b5eb46d33332e6aca4cd59fcaa2
Phil Smith <bitworxuk@gmail.com>
2024-03-07 10:39:09 +0000

    Update layout

--- config.lua
+++ config.lua
@@ -1,23 +1,18 @@
---
--- For more information on config.lua see the Project Configuration Guide at:
--- https://docs.coronalabs.com/guide/basics/configSettings
---
+local aspectRatio = display.pixelHeight / display.pixelWidth
 
-application =
+application = 
 {
-	content =
-	{
-		width = 320,
-		height = 480, 
-		scale = "letterbox",
-		fps = 60,
-		
-		--[[
-		imageSuffix =
-		{
-			    ["@2x"] = 2,
-			    ["@4x"] = 4,
-		},
-		--]]
-	},
+   content = 
+   {
+      width = aspectRatio > 1.5 and 320 or math.ceil( 480 / aspectRatio ),
+      height = aspectRatio < 1.5 and 480 or math.ceil( 320 * aspectRatio ),
+      scale = "letterBox",
+      fps = 60,
+
+      imageSuffix = 
+      {
+        ["@2x"] = 1.5,
+        --["@4x"] = 3.0,
+      },
+   },
 }
